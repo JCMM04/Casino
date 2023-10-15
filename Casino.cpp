b3aba34008e8264d6b90af7b82c4bbd8c6c55832
Darwin Chaparro <johnlandaeta1000@gmail.com>
2023-10-15 13:06:35 -0500

    Marco de ruleta añadido

--- Casino.cpp
+++ Casino.cpp
@@ -2,7 +2,25 @@
 
 using namespace std;
 
-main(){
-    //Marin me lo pelaaaaaaaaaaaaaaaaa
-    //Se debe realizar un menu
+void marcoRuleta(){
+    cout<<"     ___________________"<<endl;
+    cout<<"   /  2 3 4 5 6 7 8 9 10 \\"<<endl;
+    cout<<"  /  1                 11 \\"<<endl;
+    cout<<"* |18                   12| *"<<endl;
+    cout<<"* |19                   13| *"<<endl;
+    cout<<"* |20                   14| *"<<endl;
+    cout<<"* |31                   32| *"<<endl;
+    cout<<"* |21                   15| *"<<endl;
+    cout<<"* |22                   16| *"<<endl;
+    cout<<"* |23                   17| *"<<endl;
+    cout<<"  \\ 24 25 26 27 28 29 30 /"<<endl;
+    cout<<"   \\____________________/"<<endl;
 }
+main(){
+    int contador, opcion1, opcion2, n;
+    cout<<"Para jugar la ruleta seleccione una de los tipos de apuesta a continuacion"<<endl;
+    marcoRuleta();
+    
+    
+}
+
