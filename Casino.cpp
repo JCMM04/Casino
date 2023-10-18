d0b747955b8bb5d6e901849b6e099edae7de332d
Juan Carlos Marin <jcmarinm04@gmail.com>
2023-10-17 20:04:08 -0500

    Estructura base para generar carta al azar y calcular valor del maso, presenta un error

--- Casino.cpp
+++ Casino.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <ctime>
+#include <map>
 
 using namespace std;
 
 void menu();
+string cartaAlAzar();
+int valorMaso();
 
 main(){
     menu();
@@ -27,8 +31,14 @@ void menu(){
             // Agrega la lógica para el juego de Ruleta aquí
             break;
         case 2:
-            cout << "Iniciar el juego de Blackjack..." << endl;
-            // Agrega la lógica para el juego de Blackjack aquí
+            char generarCarta;
+            cout << "Bienvenido a Blackjack..." << endl;
+            cout<<"Carta incial... "<<endl;
+            do{
+                cout<<"El valor del maso es: "<<valorMaso(cartaAlAzar());
+                cout<<"Desea coger una carta del maso, (S/N): ";
+                cin>>generarCarta;
+            }while(generarCarta=='s' || generarCarta=='S');
             break;
         case 0:
             cout << "Saliendo del Casino. ¡Hasta pronto!" << endl;
@@ -37,4 +47,63 @@ void menu(){
             cout << "Opción no válida. Por favor, elige una opción válida." << endl;
             break;
     }
+}
+string cartaAlAzar(){
+    const string cartas[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    const string palos[] = {"Picas", "Corazon", "Diamante", "Trebol"};
+    string baraja[52];
+    bool cartaRepetida = true;
+    int indiceCarta;
+    int indicePalo;
+
+    //Evita repetir una carta y se almacena en el array baraja
+    do{
+        // Generar números aleatorios para seleccionar una carta y un palo
+        int indiceCarta = rand() % 13;
+        int indicePalo = rand() % 4;
+
+        //Recorre el array en busca de cartas iguales
+        for(int i = 0; i <52 ; i++){
+            if(!baraja[i].empty()){ //determina si el espacio esta vacio, de ser asi se sale
+                if ((cartas[indiceCarta]+palos[indicePalo])==baraja[i]){
+                    cartaRepetida=true;
+                    break;
+                }else{
+                    cartaRepetida=false;
+                    break;
+                }
+            }else{
+                baraja[i] = (cartas[indiceCarta]+palos[indicePalo]);
+                break;
+            }
+        }
+    }while(cartaRepetida);
+
+    // Mostrar la carta seleccionada al azar
+    cout << "Carta: " << cartas[indiceCarta] <<" de "<< palos[indicePalo] << endl;
+    return cartas[indiceCarta];
+}
+int valorMaso(string carta){
+    int valorMaso;
+    map<string, int> valores = {
+        {"2", 2}, 
+        {"3", 3}, 
+        {"4", 4}, 
+        {"5", 5}, 
+        {"6", 6},
+        {"7", 7}, 
+        {"8", 8}, 
+        {"9", 9}, 
+        {"10", 10}, 
+        {"J", 10},
+        {"Q", 10}, 
+        {"K", 10}
+    };
+    if((valorMaso+11)>21){
+        valores["A"] = 1;
+    }else{
+        valores["A"]= 11;
+    }
+    valorMaso+=valores[carta];
+    return valorMaso;
 }
