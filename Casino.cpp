d36efe89b3b5c3ceeb245a0cc94f23161f1201fd
Darwin Chaparro <johnlandaeta1000@gmail.com>
2023-10-19 01:39:05 -0500

    Interfaz primer fotograma hecha

--- Casino.cpp
+++ Casino.cpp
@@ -4,11 +4,21 @@
 #include<stdlib.h>
 #include<time.h>
 #include<string>
+#include <ctime>
+#include <map>
+#include <array>
+#include <iterator>
+#include <algorithm>
+
+
+
 using namespace std;
 int cTotales=5000;
 int numeroRuleta(int n, int numero, int i);
 int marcoRuleta(int numeroApostado, int cActuales);
 void menuRuleta();
+void gotoxy(int x,int y);
+void plantilla1(int n);
 main(){
     menuRuleta();
 }
@@ -39,6 +49,7 @@ int marcoRuleta(int numeroApostado, int cActuales){
     int n, bandera, contador, contador2;
     n=0;
     for(int i=0;i<resultado;i++){// hacer la animacion de la ruleta
+        //plantilla1(2);
         Sleep(100);
         system("cls");
         bandera=0;
@@ -111,19 +122,27 @@ void menuRuleta(){
         string reIntentar;
         opcion=0;
         do{
+            
+            system("cls");
             bandera=0;
             srand(time(NULL));
-            cout<<"Creditos actuales: "<<cTotales;
-            cout<<"\n     +-------------------------------------+"<<endl;
-            cout<<"     |Bienvenido a la ruleta millonaria (: |"<<endl;    
-            cout<<"     +-------------------------------------+\n"<<endl;
+            plantilla1(1);
+            gotoxy(4,1);
+            cout<<"C "<<cTotales;
+            gotoxy(30, 4);
+            cout<<"     Bienvenido a la ruleta millonaria\n"<<endl;    
             do
             {
                 if(bandera==0){
-                    cout<<"     Por favor ingrese el valor que desea apostar: ";
+                    gotoxy(28,7);
+                    cout<<"Por favor ingrese el valor que desea apostar: ";
                     cin>>vApostado;
                 }else{
-                    cout<<"     Ingrese un valor valido: ";
+                    
+                    gotoxy(28,7);
+                    cout<<"                                                  ";
+                    gotoxy(28,7);
+                    cout<<"Ingrese un valor valido: ";
                     cin>>vApostado;
                 }
                 
@@ -132,18 +151,35 @@ void menuRuleta(){
                     bandera=1;
                 }
             } while (vApostado>cTotales || vApostado<=0);
-            
+            bandera=0;
                 
-
-            cout<<"     Ingresa el numero al que deseas apostar: "<<endl;
-            cin>>apuesta[0];
-            cout<<"     ----------\n"<<endl;
-            cout<<"     |1. JUGAR|"<<endl;
-            cout<<"     |0. salir|"<<endl;
-            cout<<"     ----------\n"<<endl;
-            cout<<"     Ingrese el numero de opcion a elegir, si deseas girar la ruleta escribe Y, si deseas regresar al menu principal digita N ";
-            cin>>eleccion;
-            if (eleccion==1){
+           
+            do
+            {
+                if(bandera==0){
+                    gotoxy(28,9);
+                    cout<<"Ingresa el numero al que deseas apostar: ";
+                    cin>>apuesta[0];
+                }else{
+                    
+                    gotoxy(28,9);
+                    cout<<"                                                  ";
+                    gotoxy(28,9);
+                    cout<<"Ingrese un valor valido entre 1 y 30: ";
+                    cin>>apuesta[0];
+                }
+                
+                
+                if(apuesta[0]>30 || apuesta[0]<=0){
+                    bandera=1;
+                }
+            } while (apuesta[0]>30 || apuesta[0]<=0);
+            
+            
+            gotoxy(37, 11);
+            cout<<"DESEA GIRAR LA RULETA Y/N: ";
+            cin>>reIntentar;
+            if (reIntentar=="Y" || reIntentar=="y"){
                 opcion=marcoRuleta(apuesta[0], vApostado);
                 if(opcion==1){
                     cout<<"\n       ERES EL GANADOR";
@@ -155,8 +191,7 @@ void menuRuleta(){
                     cTotales=cTotales-vApostado;
                     //main();
                 }
-            }
-            if(eleccion==0){
+            }else{
                 main();
             }
             cout<<"\n¿Desea volver a intentarlo? Y/N: ";
@@ -171,17 +206,64 @@ void menuRuleta(){
         main();
 }
 
-void plantilla1(int foto1){
-        cout<<"+----------------------------------------------+"<<endl;
-        cout<<"|     Bienvenido a la ruleta millonaria        |"<<endl;
-        cout<<"+----------------------------------------------+"<<endl;
-        cout<<"|                                              |"<<endl;
-        cout<<"+----------------------------------------------+"<<endl;
-        cout<<"|                                              |"<<endl;
-        cout<<"|                                              |"<<endl;
-        cout<<"|                                              |"<<endl;
-        cout<<"|                                              |"<<endl;
-        cout<<"|                                              |"<<endl;
-        cout<<"|                                              |"<<endl;
-        cout<<"+----------------------------------------------+"<<endl;
+void plantilla1(int n){
+    if(n==1){
+        cout<<"                 "<<endl;
+        cout<<"                 "<<endl;
+        cout<<"    --------"<<endl;
+        cout<<"                         +-----------------------------------------------------+"<<endl;
+        cout<<"                         |                                                     |"<<endl;
+        cout<<"                         |                                                     |"<<endl;
+        cout<<"                         +-----------------------------------------------------+"<<endl;
+        cout<<"                         |                                                     |"<<endl;
+        cout<<"                         +---------------------------------------------------- +"<<endl;
+        cout<<"                         |                                                     |"<<endl;
+        cout<<"                         -------------------------------------------------------"<<endl;
+        cout<<"                         |                                                     |"<<endl;
+        cout<<"                         -------------------------------------------------------"<<endl;
+    }
+    if(n==2){
+        
+        cout<<"                          /---------------------------------------------------+"<<endl;
+        cout<<"                         /                                                     *"<<endl;
+        cout<<"                         *                                                     *"<<endl;
+        cout<<"                         *                                                     *"<<endl;
+        cout<<"                         *                                                     *"<<endl;
+        cout<<"                         *                                                     *"<<endl;
+        cout<<"                         *                                                     *"<<endl;
+        cout<<"                         *                                                     *"<<endl;
+        cout<<"                         *                                                     *"<<endl;
+        cout<<"                         *                                                     *"<<endl;
+        cout<<"                         *                                                     *"<<endl;
+        cout<<"                         *                                                     *"<<endl;
+        cout<<"                         *                                                     *"<<endl;
+        cout<<"                         *                                                     *"<<endl;
+        cout<<"                         *                                                     *"<<endl;
+        cout<<"                         *                                                      *"<<endl;
+        cout<<"                         +-----------------------------------------------------+"<<endl;
+
+    }
+/*                                      30  16  9  6  11  19  15  7  8
+
+                                        24                           25
+
+                                        12                           17
+
+                                        26                           10
+
+                                        23                           29
+
+                                        4                            18
+
+                                        13                           27
+
+                                        20  3  28  21  2  22  14  1  5*/
 }
+void gotoxy(int x,int y){  
+      HANDLE hcon;  
+      hcon = GetStdHandle(STD_OUTPUT_HANDLE);  
+      COORD dwPos;  
+      dwPos.X = x;  
+      dwPos.Y= y;  
+      SetConsoleCursorPosition(hcon,dwPos);  
+ } 
