b5c0cc1c0e3ceb1bf7fbd65951453ae20d1d5375
Juan Carlos Marin <jcmarinm04@gmail.com>
2023-10-19 02:12:11 -0500

    Version final del juego de blackJack

--- Casino.cpp
+++ Casino.cpp
@@ -89,7 +89,7 @@ void menu(){
                 }
                 
                 system("cls");
-                SinSaldo();
+                //SinSaldo();
                 imprimirMarco(2);
                 gotoxy(10,1);
                 cout<<"...Bienvenido a Blackjack..."<<endl;
@@ -114,59 +114,106 @@ void menu(){
                 valorMasoJugador=valorMaso(cartaAlAzar(),valorMasoJugador);
                 gotoxy(2,13);
                 cout<<cartaActual<<endl;
-                
+                int generadorMaso=14;
                 do{
                     valorMasoJugador=valorMaso(cartaAlAzar(),valorMasoJugador);
-                    gotoxy(2,14);
+                    gotoxy(2,generadorMaso);
+                    generadorMaso++;
                     cout<<cartaActual<<endl;
-                    gotoxy(2,15);
+                    gotoxy(25,13);
                     cout<<"Valor del maso: "<<valorMasoJugador<<endl;
                     if(valorMasoJugador<21){
-                        gotoxy(2,17);
-                        cout<<"Desea coger una carta del maso, (S/N): ";
+                        gotoxy(24,15);
+                        cout<<"Desea coger una carta"<<endl; 
+                        gotoxy(24,16);
+                        cout<<"del maso, (S/N) -> ";
                         cin>>generarCarta;
                     }
                 }while((generarCarta=='s' || generarCarta=='S') && valorMasoJugador<21);
-                cout<<"--------------------------------------------------"<<endl;
+                
+                imprimirMarco(3);
+
                 if(valorMasoJugador>21){
+                    gotoxy(51,12);
                     cout<<"TE HAS PASADO, HAS PERDIDO ESTA RONDA"<<endl;
+                    gotoxy(51,14);
                     cout<<"Tu apuesta: "<<apuesta<<endl;
+                    gotoxy(51,15);
                     cout<<"Tu nuevo saldo es: "<<calcularSaldo(apuesta,false)<<endl;
                 }else{
-                    cout<<"JUGANDO EL CRUPIER..."<<endl;
+                    imprimirMarco(4);
+                    gotoxy(62,1);
+                    cout<<"...JUGANDO EL CRUPIER..."<<endl;
+                    int generadorMaso=3;
                     do{
                         valorMasoCrupier=valorMaso(cartaAlAzar(),valorMasoCrupier);
+                        gotoxy(51,generadorMaso);
+                        generadorMaso++;
                         cout<<cartaActual<<endl;
+                        gotoxy(2,10);
+                        cout<<"                  "<<endl;
                     }while(valorMasoCrupier<17);
+                    gotoxy(66,3);
                     cout<<"Valor del maso del crupier: "<<valorMasoCrupier<<endl;
-
                     if(valorMasoCrupier<=21){
                         if(valorMasoJugador>valorMasoCrupier){
+                            gotoxy(51,12);
                             cout<<"HAS GANADO ESTA RONDA"<<endl;
+                            gotoxy(51,14);
                             cout<<"Tu apuesta: "<<apuesta<<endl;
+                            gotoxy(51,15);
                             cout<<"Tu nuevo saldo es: "<<calcularSaldo(apuesta,true)<<endl;
                         }else if(valorMasoJugador<valorMasoCrupier){
+                            gotoxy(51,12);
                             cout<<"HAS PERDIDO ESTA RONDA"<<endl;
+                            gotoxy(51,14);
                             cout<<"Tu apuesta: "<<apuesta<<endl;
+                            gotoxy(51,15);
                             cout<<"Tu nuevo saldo es: "<<calcularSaldo(apuesta,false)<<endl;
                         }else{
+                            gotoxy(51,12);
                             cout<<"HAS EMPATADO ESTA RONDA"<<endl;
+                            gotoxy(51,14);
                             cout<<"Tu apuesta: "<<apuesta<<endl;
+                            gotoxy(51,15);
                             cout<<"Tu nuevo saldo es: "<<saldo<<endl;
                         }
                     }else{
+                        gotoxy(51,12);
                         cout<<"HAS GANADO ESTA RONDA"<<endl;
+                        gotoxy(51,14);
                         cout<<"Tu apuesta: "<<apuesta<<endl;
+                        gotoxy(51,15);
                         cout<<"Tu nuevo saldo es: "<<calcularSaldo(apuesta,true)<<endl;
                     }
                 }
-                cout<<"Desea jugar una nueva ronda (S/N): "<<endl;
-                cin>>nuevaRonda;
-            }while(nuevaRonda=='S' || nuevaRonda=='s');
+                if(saldo<=0){
+                    int nuevoSaldo;
+                    gotoxy(75,14);
+                    cout<<"Debes ingresar"; 
+                    gotoxy(75,15);
+                    cout<<"un nuevo saldo";
+                    gotoxy(51,17);
+                    cout<<"1.Ingresar saldo / 2.Salir del casino -> "<<endl;
+                    gotoxy(92,17);
+                    cin>>nuevoSaldo;
+                    if(nuevoSaldo==2){
+                        gotoxy(0,20);
+                        cout<<"Saliendo del casino, vuelva pronto...";
+                        exit(3);
+                    }
+                }else{
+                    gotoxy(51,17);
+                    cout<<"Desea jugar una nueva ronda (S/N) -> "<<endl;
+                    gotoxy(88,17);
+                    cin>>nuevaRonda;
+                }
+            }while((nuevaRonda=='S' || nuevaRonda=='s') && saldo>0);
             menu();
             break;
         case 0:
-            cout << "Saliendo del Casino. ¡Hasta pronto!" << endl;
+            gotoxy(0,13);
+            cout << "Saliendo del Casino. Hasta pronto!" << endl;
             break; // Salir del programa
         default:
             cout << "Opción no válida. Por favor, elige una opción válida." << endl;
@@ -272,17 +319,29 @@ double validarApuestas(double monto){
     bool montoInvalido = true;
     do{
         if(monto<=0){
+            gotoxy(49,5);
             cout<<"Monto invalido, ingrese una suma mayor a 0$ y menor a su saldo: "<<saldo<<"$"<<endl;
+            gotoxy(21,5);
+            cout<<"              ";
+            gotoxy(21,5);
             cin>>monto;
         }else{
             if(monto<=saldo){
                 montoInvalido=false;
             }else{
+                gotoxy(49,5);
+                cout<<"                                                                            ";
+                gotoxy(49,5);
                 cout<<"Ingrese un valor menor o igual a su saldo: "<<saldo<<"$"<<endl;
+                gotoxy(21,5);
+                cout<<"              ";
+                gotoxy(21,5);
                 cin>>monto;
             }
         }
     }while(montoInvalido);
+    gotoxy(49,5);
+    cout<<"                                                                            ";
     return monto;
 }
 void SinSaldo(){
@@ -304,13 +363,19 @@ void SinSaldo(){
 void validarSaldo(){
     bool saldoInvalido = true;
     do{
-        if(saldo<0){
+        if(saldo<=0){
+            gotoxy(49,3);
             cout<<"Ingrese un saldo mayor a 0$: "<<endl;
+            gotoxy(30,3);
+            cout<<"                ";
+            gotoxy(30,3);
             cin>>saldo;
         }else{
             saldoInvalido = false;
         }
     }while(saldoInvalido);
+    gotoxy(49,3);
+    cout<<"                             ";
 }
 void gotoxy(int x,int y){  
       HANDLE hcon;  
@@ -348,9 +413,53 @@ void imprimirMarco(int opcion){
         cout<<"|                                              |"<<endl;
         cout<<"+----------------------------------------------+"<<endl;
         cout<<"|                                              |"<<endl;
-        cout<<"|                                              |"<<endl;
-        cout<<"|                                              |"<<endl;
-        cout<<"|                                              |"<<endl;
+        cout<<"|                    |                         |"<<endl;
+        cout<<"|                    |                         |"<<endl;
+        cout<<"|                    |                         |"<<endl;
+        cout<<"|                    |                         |"<<endl;
+        cout<<"|                    |                         |"<<endl;
+        cout<<"|                    |                         |"<<endl;
         cout<<"+----------------------------------------------+"<<endl;
+    }else if(opcion== 3){
+        gotoxy(50,11);
+        cout<<"+----------------------------------------------+"<<endl;
+        gotoxy(50,12);
+        cout<<"|                                              |"<<endl;
+        gotoxy(50,13);
+        cout<<"+----------------------------------------------+"<<endl;
+        gotoxy(50,14);
+        cout<<"|                                              |"<<endl;
+        gotoxy(50,15);
+        cout<<"|                                              |"<<endl;
+        gotoxy(50,16);
+        cout<<"+----------------------------------------------+"<<endl;
+        gotoxy(50,17);
+        cout<<"|                                              |"<<endl;
+        gotoxy(50,18);
+        cout<<"+----------------------------------------------+"<<endl;
+    }else if(opcion==4){
+        gotoxy(50,0);
+        cout<<"+----------------------------------------------+"<<endl;
+        gotoxy(50,1);
+        cout<<"|                                              |"<<endl;
+        gotoxy(50,2);
+        cout<<"+----------------------------------------------+"<<endl;
+        gotoxy(50,3);
+        cout<<"|              |                               |"<<endl;
+        gotoxy(50,4);
+        cout<<"|              |                               |"<<endl;
+        gotoxy(50,5);
+        cout<<"|              |                               |"<<endl;
+        gotoxy(50,6);
+        cout<<"|              |                               |"<<endl;
+        gotoxy(50,7);
+        cout<<"|              |                               |"<<endl;
+        gotoxy(50,8);
+        cout<<"|              |                               |"<<endl;
+        gotoxy(50,9);
+        cout<<"|              |                               |"<<endl;
+        gotoxy(50,10);
+        cout<<"|              |                               |"<<endl;
+        
     }
 }
